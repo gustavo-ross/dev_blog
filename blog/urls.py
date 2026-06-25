c864722aa34ffb82766f17fac30779b4ddc1a767
Luis Gustavo dos Santos da Rosa <gutosantos17.lg@gmail.com>
2026-06-25 20:40:57 -0300

    Aula quinta 25/06 - slide 2

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('sobre/', views.sobre_nos, name='Sobre nós'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    mensagem = "<h1>Bem-Vindo ao Devblog!</h1> <p>Em breve, artigos aqui.</p>"
+
+    return HttpResponse(mensagem)
+
+
+def sobre_nos(request):
+    mensagem = "<h1>Sobre o Devblog!</h1> <p>Ainda nem existe, então não tem muito o que falar.</p>"
+
+    return HttpResponse(mensagem)
