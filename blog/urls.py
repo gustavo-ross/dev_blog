e6fe158cfd1a26c0c392fb4f956c53db52dfa006
Luis Gustavo dos Santos da Rosa <gutosantos17.lg@gmail.com>
2026-06-30 20:06:56 -0300

    desafio foto

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('sobre/', views.sobre_nos, name='Sobre nós'),
+    path('sobre/', views.sobre_nos, name='sobre_nos'),
 ]
