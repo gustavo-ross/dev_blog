31664e0d5361194aec0f80a102de53c8af0c916d
Luis Gustavo dos Santos da Rosa <gutosantos17.lg@gmail.com>
2026-06-30 21:05:43 -0300

    Aula 69 + Desafio - Models e Migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+    
+
+class Artigo(models.Model):
+    titulo = models.CharField(max_length=200)
+    conteudo = models.TextField()
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    autor = models.CharField(max_length=50, default="Admin")
+
+    def __str__(self):
+        return self.titulo
