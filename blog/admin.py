aa61fc26e071b6617151dd66dfdc4075709ef4fa
Luis Gustavo dos Santos da Rosa <gutosantos17.lg@gmail.com>
2026-07-01 20:48:12 -0300

    Aula 4 - 70 e 79

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Categoria, Artigo
 
-# Register your models here.
+admin.site.register(Categoria)
+
+@admin.register(Artigo)
+class ArtigoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'categoria', 'data_publicacao')
+    search_fields = ('titulo', 'conteudo')
+    list_filter = ('categoria', 'data_publicacao')
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Artigo
 
 def home(request):
-    return render(request, 'blog/index.html')
+    noticias = Artigo.objects.all()
+    contexto = {
+        'lista_artigos': noticias
+    }
+    return render(request, 'blog/index.html', contexto)
 
 
 def sobre_nos(request):
