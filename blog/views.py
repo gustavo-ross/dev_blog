fd836bc98079716adf41f83a68d286dc16d3ae2a
Luis Gustavo dos Santos da Rosa <gutosantos17.lg@gmail.com>
2026-07-01 20:56:19 -0300

    Checkpoint 7

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Artigo
+from .models import Artigo, Categoria
 
 def home(request):
-    noticias = Artigo.objects.all()
+    # Captura o ID da categoria pela URL (ex: /?categoria=1)
+    categoria_id = request.GET.get('categoria')
+    
+    # Se existe um ID na URL, filtramos. Se não, mostramos tudo.
+    if categoria_id:
+        noticias = Artigo.objects.filter(categoria_id=categoria_id)
+        # Pegamos a categoria específica para mandar para o título
+        categoria_atual = Categoria.objects.filter(id=categoria_id).first()
+    else:
+        noticias = Artigo.objects.all()
+        categoria_atual = None
+
+    # As categorias do menu precisam carregar sempre
+    categorias = Categoria.objects.all()
+    
     contexto = {
-        'lista_artigos': noticias
+        'lista_artigos': noticias,
+        'lista_categorias': categorias,
+        'categoria_atual': categoria_atual, # Enviamos a categoria filtrada para o HTML
     }
     return render(request, 'blog/index.html', contexto)
 
 
 def sobre_nos(request):
-    return render(request, 'blog/sobre.html')
+    # Para o menu dinâmico aparecer na página Sobre, a view também precisa enviar os dados!
+    categorias = Categoria.objects.all()
+    contexto = {
+        'lista_categorias': categorias
+    }
+    return render(request, 'blog/sobre.html', contexto)
